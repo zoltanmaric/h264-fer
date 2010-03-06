95299ddd5a99d2cb7b1aa7ca1fffca446fbd36b0
zoltaaaan@gmail.com <zoltaaaan@gmail.com@9f3bdf0a-daf3-11de-b590-7f2b5bfbe1fd>
2010-03-06 15:45:47 +0000

    Ispravljen writeToPPM, sad se crta u čitavu sliku.

--- trunk/fer_h264/fer_h264/writeToPPM.cpp
+++ trunk/fer_h264/fer_h264/writeToPPM.cpp
@@ -3,23 +3,23 @@
 
 #include "h264_globals.h"
 	
-unsigned char *r;
-unsigned char *g;
-unsigned char *b;
+unsigned char *red;
+unsigned char *green;
+unsigned char *blue;
 
 void toRGB()
 {
-	r = new unsigned char[frame.Lwidth * frame.Lheight];
-	g = new unsigned char[frame.Lwidth * frame.Lheight];
-	b = new unsigned char[frame.Lwidth * frame.Lheight];
+	red = new unsigned char[frame.Lwidth * frame.Lheight];
+	green = new unsigned char[frame.Lwidth * frame.Lheight];
+	blue = new unsigned char[frame.Lwidth * frame.Lheight];
 
 	for (int i = 0; i < frame.Lheight; i++)
 	{
 		for (int j = 0; j < frame.Lwidth; j++)
 		{
-			r[i*frame.Lwidth + j] = 1.164*(frame.L[i*frame.Lwidth + j] - 16) + 1.596*(frame.C[1][(i/2)*frame.Cwidth + (j/2)] - 128);
-			g[i*frame.Lwidth + j] = 1.164*(frame.L[i*frame.Lwidth + j] - 16) - 0.813*(frame.C[1][(i/2)*frame.Cwidth + (j/2)] - 128) - 0.391*(frame.C[0][(i/2)*frame.Cwidth + (j/2)] - 128);
-			b[i*frame.Lwidth + j] = 1.164*(frame.L[i*frame.Lwidth + j] - 16) + 2.018*(frame.C[0][(i/2)*frame.Cwidth + (j/2)] - 128);
+			red[i*frame.Lwidth + j] = 1.164*(frame.L[i*frame.Lwidth + j] - 16) + 1.596*(frame.C[1][(i/2)*frame.Cwidth + (j/2)] - 128);
+			green[i*frame.Lwidth + j] = 1.164*(frame.L[i*frame.Lwidth + j] - 16) - 0.813*(frame.C[1][(i/2)*frame.Cwidth + (j/2)] - 128) - 0.391*(frame.C[0][(i/2)*frame.Cwidth + (j/2)] - 128);
+			blue[i*frame.Lwidth + j] = 1.164*(frame.L[i*frame.Lwidth + j] - 16) + 2.018*(frame.C[0][(i/2)*frame.Cwidth + (j/2)] - 128);
 		}
 	}
 }
@@ -36,11 +36,11 @@ void writeToPPM()
 	int k = 0;
 	for (int i = 0; i < frame.Lwidth * frame.Lheight; i++)
 	{
-		slika[k++] = r[i];
-		slika[k++] = g[i];
-		slika[k++] = b[i];
+		slika[k++] = red[i];
+		slika[k++] = green[i];
+		slika[k++] = blue[i];
 	}
 
-	fwrite(slika, 1, frame.Lwidth * frame.Lheight, f);
+	fwrite(slika, 1, frame.Lwidth * frame.Lheight * 3, f);
 	fclose(f);
 }
