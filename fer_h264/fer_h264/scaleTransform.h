1bd9b2e2c68341022af4dfb1ed1be447a6b00d46
zoltaaaan@gmail.com <zoltaaaan@gmail.com@9f3bdf0a-daf3-11de-b590-7f2b5bfbe1fd>
2010-03-08 18:35:17 +0000

    Prčkam po Haramustekovom kodu. Pišem pomoćne funkcije.

--- fer_h264/fer_h264/scaleTransform.h
+++ fer_h264/fer_h264/scaleTransform.h
@@ -5,6 +5,7 @@
 
 extern int ZigZagReordering[16][2];
 
+void transformInverseScan(int list[16], int c[4][4]);
 void inverseResidual(int bitDepth, int qP, int c[4][4], int r[4][4], bool intra16x16OrChroma);
 void InverseDCLumaIntra (int bitDepth, int qP, int c[4][4], int dcY[4][4]);
 void InverseDCChroma (int bitDepth, int qP, int c[2][2], int dcC[2][2]);
