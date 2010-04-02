43e96506de58e55f9025fa24a581c1d325fc7309
zoltaaaan@gmail.com <zoltaaaan@gmail.com@9f3bdf0a-daf3-11de-b590-7f2b5bfbe1fd>
2010-04-02 17:55:19 +0000

    Neki ispravci u signed exp-golomp kodiranju.

--- fer_h264/fer_h264/expgolomb.h
+++ fer_h264/fer_h264/expgolomb.h
@@ -5,7 +5,7 @@ unsigned int expGolomb_UD();
 signed int expGolomb_SD();
 
 void expGolomb_UC(unsigned int codeNum);
-void expGolomb_SC(unsigned int codeNum);
+void expGolomb_SC(int codeNum);
 
 void golombRice_SC(int codeNum, unsigned int VLCNum);
 
